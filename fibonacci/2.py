b7abf367f9e40f5f403b0749c9af22feabecb386
ancient-clever <ancient-clever@outlook.com>
2019-08-11 00:06:17 +0300

    last digit in big number

---
+++ fibonacci/2.py
@@ -0,0 +1,15 @@
+def fib_digit(n):
+    last, current = 0, 1
+    for r in range(1, n):
+        last, current = current, (last + current) % 10
+
+    return current % 10
+
+
+def main():
+    n = int(input())
+    print(fib_digit(n))
+
+
+if __name__ == "__main__":
+    main()

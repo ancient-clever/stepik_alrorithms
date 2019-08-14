762869dabd21f0cec61338163dbed46064ec057b
ancient-clever <ancient-clever@outlook.com>
2019-08-14 19:57:26 +0300

    practice video 3

--- practice/1.py
+++ practice/1.py
@@ -11,3 +11,22 @@ def fib2(n):
     if n not in cache:
         cache[n] = n if n <= 1 else fib2(n-1) + fib2(n-2)
     return cache[n]
+
+
+def memo(f):
+    cache = {}
+
+    def inner(n):
+        if n not in cache:
+            cache[n] = f[n]
+        return cache[n]
+    return inner
+
+
+new_fib1 = memo(fib1)
+
+
+from functools import lru_cache
+
+
+fib3 = lru_cache(maxsize=None)(fib1)

40fd791f275a9609f7b3c90db704b8181994cee6
ancient-clever <ancient-clever@outlook.com>
2019-08-14 20:09:24 +0300

    practice video 5

--- practice/1.py
+++ practice/1.py
@@ -38,3 +38,32 @@ def fib3(n):
     for i in range(n-1):
         f0, f1 = f1, f0 + f1
     return f1
+
+
+import time
+
+
+def timed(f, *args, n_iter=100):
+    acc = float("inf")
+    for i in range(n_iter):
+        t0 = time.perf_counter()
+        f(*args)
+        t1 = time.perf_counter()
+        acc = min(acc, t1 - t0)
+    return acc
+
+
+timed(fib3, 800)
+
+
+from matplotlib import pyplot as plt
+
+
+def compare(fs, args):
+    for f in fs:
+        plt.plot(args, [timed(f, arg) for arg in args], label=f.__name__)
+    plt.leggend()
+    plt.grid(True)
+
+
+compare([fib1, fib3], list(range(20)))

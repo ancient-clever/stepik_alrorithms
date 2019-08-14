aba995c1dd070cee6daea6d58341b2bcc4d3ac8c
ancient-clever <ancient-clever@outlook.com>
2019-08-14 19:51:17 +0300

    practice video 2

--- practice/1.py
+++ practice/1.py
@@ -1,3 +1,13 @@
 def fib1(n):
     assert n >= 0
     return n if n <= 1 else fib1(n-1) + fib1(n-2)
+
+
+cache = {}
+
+
+def fib2(n):
+    assert n >= 0
+    if n not in cache:
+        cache[n] = n if n <= 1 else fib2(n-1) + fib2(n-2)
+    return cache[n]

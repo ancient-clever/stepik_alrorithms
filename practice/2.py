7844c5a6da8b896098d0aa985170b7596af38d1c
ancient-clever <ancient-clever@outlook.com>
2019-08-15 20:57:18 +0300

    practice video 2

--- practice/2.py
+++ practice/2.py
@@ -12,3 +12,9 @@ def test(gcd, n_iter=100):
         d = gcd(a, b)
         assert a % d == b % d == 0
 
+
+def gcd1(a, b):
+    assert a >= 0 and b >= 0
+    for d in reversed(range(max(a, b) + 1)):
+        if d == 0 or a % d == b % d == 0:
+            return d

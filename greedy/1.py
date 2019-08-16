72ed31eb1148e0b11ed753913a1e74a558f0af1f
ancient-clever <ancient-clever@outlook.com>
2019-08-16 23:09:07 +0300

    native

--- greedy/1.py
+++ greedy/1.py
@@ -1,14 +1,25 @@
-def points(sections):
-    pass
+def cross_points(sections):
+    # провкряьт лежит ли точка на отрезке
+    points = []
+    while sections:
+        a, b = sections.pop()
+        for c, d in sections:
+            if a <= c <= b:
+                points.append(c)
+            elif c <= a <= d:
+                points.append(a)
+
+    return points
 
 
 def main():
-    n = int(input())
-    sections = []
-    for r in range(n):
-        section = input().split()
-        sections.append(section)
-    # print(points(sections))
+    # n = int(input())
+    # sections = []
+    # for r in range(n):
+    #     section = input().split()
+    #     sections.append(section)
+    # print(cross_points(sections))
+    print(cross_points([['1', '3'], ['2', '5']]))
 
 
 if __name__ == "__main__":

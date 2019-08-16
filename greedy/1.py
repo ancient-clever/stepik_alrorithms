63558750b9babe035adf257e616ec05f300c6296
ancient-clever <ancient-clever@outlook.com>
2019-08-16 20:08:39 +0300

    initial

---
+++ greedy/1.py
@@ -0,0 +1,15 @@
+def points(sections):
+    pass
+
+
+def main():
+    n = int(input())
+    sections = []
+    for r in range(n):
+        section = input().split()
+        sections.append(section)
+    # print(points(sections))
+
+
+if __name__ == "__main__":
+    main()

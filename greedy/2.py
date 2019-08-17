1b289658535d4c59efe454677f3918a616d214d1
ancient-clever <ancient-clever@outlook.com>
2019-08-18 01:02:08 +0300

    final

--- greedy/2.py
+++ greedy/2.py
@@ -2,7 +2,18 @@ from operator import itemgetter
 
 
 def fit_price(pack_size, items):
-    pass
+    total = 0
+    while items:
+        if pack_size <= 0:
+            break
+        price, size, value = items.pop(0)
+        if size <= pack_size:
+            total += price
+            pack_size -= size
+        else:
+            total += pack_size*value
+            pack_size = 0
+    return format(float(total), '.3f')
 
 
 def main():
@@ -11,7 +22,7 @@ def main():
     for r in range(int(n)):
         price, size = input().split()
         items.append([int(price), int(size), int(price)/int(size)])
-    print(fit_price(pack_size, sorted(items, key=itemgetter(2), reverse=True)))
+    print(fit_price(int(pack_size), sorted(items, key=itemgetter(2), reverse=True)))
 
 
 if __name__ == "__main__":

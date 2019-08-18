33b9d1c3aa44316920d105115270323e3c91bed2
ancient-clever <ancient-clever@outlook.com>
2019-08-18 16:52:29 +0300

    final

--- greedy/3.py
+++ greedy/3.py
@@ -1,10 +1,22 @@
 def find_term(number):
-    pass
+    result = []
+    step = 1
+    while number:
+        if number - step >= 0:
+            number -= step
+            result.append(step)
+            step += 1
+        else:
+            last = result.pop()
+            number += last
+    return result
 
 
 def main():
     n = int(input())
-    print(find_term(n))
+    result = find_term(n)
+    print(len(result))
+    print(' '.join(str(point) for point in result))
 
 
 if __name__ == "__main__":

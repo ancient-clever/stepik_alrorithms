7ebf2ace7b4d7c09275ca3501912b8f282b01ef3
ancient-clever <ancient-clever@outlook.com>
2019-09-01 22:39:34 +0300

    fast search initial

---
+++ fast_search/1.py
@@ -0,0 +1,11 @@
+def main():
+    n, m = [int(i) for i in input().split()]
+    lines = []
+    for r in range(n):
+        line = [int(i) for i in input().split()]
+        lines.append(line)
+    points = [int(i) for i in input().split()]
+
+
+if __name__ == "__main__":
+    main()

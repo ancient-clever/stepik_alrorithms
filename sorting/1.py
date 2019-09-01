7f9fdf9db9c2096759139c9bda38e9394c5fecf6
ancient-clever <ancient-clever@outlook.com>
2019-09-01 18:19:19 +0300

    naive inversion

--- sorting/1.py
+++ sorting/1.py
@@ -1,9 +1,18 @@
-import sys
+def count_pairs(arr, n):
+    inv_count = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            if (arr[i] > arr[j]):
+                inv_count += 1
+
+    return inv_count
 
 
 def main():
-    n = int(sys.stdin)
-    row = [int(number) for number in sys.stdin.split()]
+    n = int(input())
+    row = [int(number) for number in input().split()]
+    pairs = count_pairs(row, n)
+    print(pairs)
 
 
 if __name__ == "__main__":

67b45ffb58f6eaa04e32b2c67568bd43e429923c
ancient-clever <ancient-clever@outlook.com>
2019-09-01 18:24:46 +0300

    mergesort inversion

--- sorting/1.py
+++ sorting/1.py
@@ -1,9 +1,49 @@
-def count_pairs(arr, n):
+def merge_sort(arr, n):
+    temp_arr = [0] * n
+    return _merge_sort(arr, temp_arr, 0, n - 1)
+
+
+def _merge_sort(arr, temp_arr, left, right):
     inv_count = 0
-    for i in range(n):
-        for j in range(i + 1, n):
-            if (arr[i] > arr[j]):
-                inv_count += 1
+
+    if left < right:
+        mid = (left + right) // 2
+        inv_count = _merge_sort(arr, temp_arr, left, mid)
+        inv_count += _merge_sort(arr, temp_arr, mid + 1, right)
+        inv_count += merge(arr, temp_arr, left, mid, right)
+    return inv_count
+
+
+def merge(arr, temp_arr, left, mid, right):
+    i = left
+    j = mid + 1
+    k = left
+    inv_count = 0
+
+    while i <= mid and j <= right:
+
+        if arr[i] <= arr[j]:
+            temp_arr[k] = arr[i]
+            k += 1
+            i += 1
+        else:
+            temp_arr[k] = arr[j]
+            inv_count += (mid - i + 1)
+            k += 1
+            j += 1
+
+    while i <= mid:
+        temp_arr[k] = arr[i]
+        k += 1
+        i += 1
+
+    while j <= right:
+        temp_arr[k] = arr[j]
+        k += 1
+        j += 1
+
+    for loop_var in range(left, right + 1):
+        arr[loop_var] = temp_arr[loop_var]
 
     return inv_count
 
@@ -11,7 +51,7 @@ def count_pairs(arr, n):
 def main():
     n = int(input())
     row = [int(number) for number in input().split()]
-    pairs = count_pairs(row, n)
+    pairs = merge_sort(row, n)
     print(pairs)
 
 

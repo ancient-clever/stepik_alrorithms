2bbb1fc308df1313eddca647ed10bbf336adf009
ancient-clever <ancient-clever@outlook.com>
2019-09-02 20:43:53 +0300

    quick sort final

--- fast_search/1.py
+++ fast_search/1.py
@@ -1,47 +1,41 @@
-def quick(n, left, right, points):
-    for i in points:
-        point = int(i)
+n, m = map(int, input().split())
+left, right = [], []
 
-        beg, end = 0, n - 1
-        while end >= beg:
-            j = (beg + end) // 2
-            if left[j] > point:
-                if j == beg or left[j - 1] <= point:
-                    break
-                else:
-                    if j == end:
-                        j += 1
-                    end = j - 1
+for i in range(n):
+    l, r = map(int, input().split())
+    left.append(int(l))
+    right.append(int(r))
+
+left.sort()
+right.sort()
+
+for i in input().split():
+    point = int(i)
+
+    beg, end = 0, n - 1
+    while end >= beg:
+        j = (beg + end) // 2
+        if left[j] > point:
+            if  j == beg or left[j - 1] <= point:
+                break
             else:
-                beg = j + 1
-                j = j + 1
+                if j == end:
+                    j += 1
+                end = j - 1
+        else:
+            beg = j + 1
+            j = j + 1
 
-        beg, end = 0, n - 1
-        while end >= beg:
-            k = (beg + end) // 2
-            if right[k] < point:
-                if k == end:
-                    k += 1
-                beg = k + 1
-            else:
-                if k != beg and right[k - 1] >= point:
-                    end = k - 1
-                else:
-                    break
+    beg, end = 0, n - 1
+    while end >= beg:
+        k = (beg + end) // 2
+        if right[k] < point:
+            if k == end:
+                k += 1
+            beg = k + 1
+        else:
+            if k != beg and right[k - 1] >= point:
+                end = k - 1
+            else: break
 
-        print(j - k, end=" ")
-
-
-def main():
-    n, m = map(int, input().split())
-    left, right = [], []
-    for r in range(n):
-        l, r = map(int, input().split())
-        left.append(l)
-        right.append(r)
-    points = map(int, input().split())
-    quick(n, left, right, points)
-
-
-if __name__ == "__main__":
-    main()
+    print(j - k, end = " ")
